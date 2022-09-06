865b5f824315af2a7b7640145dd7a67548b6cf80
Ghassen <dhiebghassen@gmail.com>
2022-09-06 21:11:07 +0100

    Add modes buttons and activate them

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const DEFAULT_VALUE = 16;
-const DEFAULT_COLOR = "#fff";
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_MODE = "color";
 
 let currentSize = DEFAULT_VALUE;
 let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
 
 let myGrid = document.querySelector(".grid");
+let colorPicker = document.getElementById("colorPicker");
+let colorMode = document.getElementById("colorMode");
+let rainbowMode = document.getElementById("rainbowMode");
+let eraser = document.getElementById("eraser");
+let clearBtn = document.getElementById("clearBtn");
+
+let sizeSlider = document.getElementById("sizeSlider");
+let sizeValue = document.getElementById("sizeValue");
+
 let rows = document.getElementsByClassName("rows");
 let cols = document.getElementsByClassName("cols");
 
@@ -14,6 +25,33 @@ function setSize(newSize) {
 
 function setCurrentColor(newColor) {
   currentColor = newColor;
+}
+
+function setCurrentMode(newMode) {
+  activateButton(newMode);
+  currentMode = newMode;
+}
+
+function activateButton(newMode) {
+  if ((currentMode = "rainbow")) {
+    rainbowMode.classList.remove("active");
+  }
+  if ((currentMode = "color")) {
+    colorMode.classList.remove("active");
+  }
+  if ((currentMode = "eraser")) {
+    eraser.classList.remove("active");
+  }
+
+  if (newMode == "rainbow") {
+    rainbowMode.classList.add("active");
+  }
+  if (newMode == "color") {
+    colorMode.classList.add("active");
+  }
+  if (newMode == "eraser") {
+    eraser.classList.add("active");
+  }
 }
 
 function makeRow(rowNum) {
@@ -29,22 +67,22 @@ function makeCol(colNum) {
     for (let j = 0; j < colNum; j++) {
       let col = document.createElement("div");
       col.classList.add("cols");
-      col.addEventListener("mousedown", changeColor);
+
       col.addEventListener("mouseover", changeColor);
+      col.addEventListener("mousedown", changeColor);
+
       rows[j].appendChild(col);
     }
   }
 }
 
-let colorPicker = document.getElementById("colorPicker");
-let clearBtn = document.getElementById("clearBtn");
-let sizeSlider = document.getElementById("sizeSlider");
-let sizeValue = document.getElementById("sizeValue");
-
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
 clearBtn.onclick = () => reloadGrid();
 colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorMode.onclick = () => setCurrentMode("color");
+rainbowMode.onclick = () => setCurrentMode("rainbow");
+eraser.onclick = () => setCurrentMode("eraser");
 
 function updateSizeValue(value) {
   sizeValue.innerHTML = `${value} X ${value}`;
@@ -74,11 +112,9 @@ document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 function changeColor(e) {
-  if ((e.type = "mouseover" && !mouseDown)) return;
+  if (e.type === "mouseover" && !mouseDown) return;
 
-  if ((e.type = "mouseover" && mouseDown)) {
-    e.target.style.backgroundColor = currentColor;
-  }
+  e.target.style.backgroundColor = currentColor;
 }
 
 window.onload = () => changeSize(DEFAULT_VALUE);
