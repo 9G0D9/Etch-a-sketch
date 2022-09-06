ff7fd0b9b13deaaf67203ff17ab0774785cb7ce1
Ghassen <dhiebghassen@gmail.com>
2022-09-06 19:03:21 +0100

    Change the color of the grid colums

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const DEFAULT_VALUE = 16;
+const DEFAULT_COLOR = "#fff";
 
 let currentSize = DEFAULT_VALUE;
+let currentColor = DEFAULT_COLOR;
 
 let myGrid = document.querySelector(".grid");
 let rows = document.getElementsByClassName("rows");
 let cols = document.getElementsByClassName("cols");
 
-function setCurrentSize(newSize) {
+function setSize(newSize) {
   currentSize = newSize;
+}
+
+function setCurrentColor(newColor) {
+  currentColor = newColor;
 }
 
 function makeRow(rowNum) {
@@ -23,25 +29,29 @@ function makeCol(colNum) {
     for (let j = 0; j < colNum; j++) {
       let col = document.createElement("div");
       col.classList.add("cols");
+      col.addEventListener("mousedown", changeColor);
+      col.addEventListener("mouseover", changeColor);
       rows[j].appendChild(col);
     }
   }
 }
 
+let colorPicker = document.getElementById("colorPicker");
+let clearBtn = document.getElementById("clearBtn");
 let sizeSlider = document.getElementById("sizeSlider");
 let sizeValue = document.getElementById("sizeValue");
-let clearBtn = document.getElementById("clearBtn");
 
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
 clearBtn.onclick = () => reloadGrid();
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
 
 function updateSizeValue(value) {
   sizeValue.innerHTML = `${value} X ${value}`;
 }
 
 function changeSize(value) {
-  setCurrentSize(value);
+  setSize(value);
   reloadGrid(currentSize);
 }
 
@@ -59,6 +69,16 @@ function clearGrid() {
   myGrid.innerHTML = "";
 }
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
+function changeColor(e) {
+  if ((e.type = "mouseover" && !mouseDown)) return;
+
+  if ((e.type = "mouseover" && mouseDown)) {
+    e.target.style.backgroundColor = currentColor;
+  }
+}
 
 window.onload = () => changeSize(DEFAULT_VALUE);
